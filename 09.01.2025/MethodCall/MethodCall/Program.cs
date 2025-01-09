cccad6888697ddf291aef9335e57da7daa6ca152
Opilane TTHK <opilane@tthk.ee>
2025-01-09 14:43:43 +0200

    init

---
+++ 09.01.2025/MethodCall/MethodCall/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace MethodCall
+{
+    internal class Program
+    {
+        static void MinuMeetod()
+        {
+            Console.WriteLine("Meetod! Meetod! Meetod!");
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Siin näed minu meetodit:");
+            MinuMeetod();
+            MinuMeetod();
+            MinuMeetod();
+
+        }
+        
+    }
+}
