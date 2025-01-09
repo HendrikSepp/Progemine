63ccf69614969c0c8baad114db256122e8754fe7
Opilane TTHK <opilane@tthk.ee>
2025-01-09 13:54:38 +0200

    init

---
+++ IfAndElse/IfAndElse/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace IfAndElse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta palun vanus");
+            int vanus = int.Parse(Console.ReadLine());
+
+            if (vanus <= 17)
+            {
+                Console.WriteLine("Oled alaealine!");
+            }
+            else if (vanus >= 18 && vanus <= 64)
+            {
+                Console.WriteLine("Oled täisealine!");
+            }
+            else
+            {
+                Console.WriteLine("Oled pensionär!");
+            }
+        }
+    }
+}
---
+++ IfElseNesting/IfElseNesting/IfElseNesting/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace IfElseNesting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("If ja else nestimine");
+
+            if ()
+            {
+                if ()
+                {
+
+                }
+                else
+                {
+
+                }
+            }
+            {
+
+            }
+
+        }
+    }
+}
