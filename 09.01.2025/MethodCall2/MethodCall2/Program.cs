081bffa6c502d64653b30f65af192fee5a44ff1e
Opilane TTHK <opilane@tthk.ee>
2025-01-09 14:57:54 +0200

    init

---
+++ 09.01.2025/MethodCall2/MethodCall2/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace MethodCall2
+{
+    internal class Program
+    {
+        static void Paaritu()
+        {
+            Console.WriteLine("Arv on paaritu!");
+        }
+        static void Paaris()
+        {
+            Console.WriteLine("Arv on paaris!");
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta arv");
+            int number = int.Parse(Console.ReadLine());
+            if (number % 2 == 1)
+            {
+                Paaritu();
+            }
+            else
+            {
+                Paaris();
+            }
+        }
+        
+    }
+}
