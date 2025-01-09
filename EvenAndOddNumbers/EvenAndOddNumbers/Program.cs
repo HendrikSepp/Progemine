2bdd3093a95d21404d6e70b2041c9a1358d307e7
Opilane TTHK <opilane@tthk.ee>
2025-01-09 14:24:18 +0200

    init

---
+++ EvenAndOddNumbers/EvenAndOddNumbers/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace EvenAndOddNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta arv");
+            string nr = Console.ReadLine();
+            int number = int.Parse(nr);
+
+            if (number % 2 == 1)
+            {
+                Console.WriteLine("arv on paaritu!");
+            }
+            else
+            {
+                Console.WriteLine("arv on paaris!");
+            }
+        }
+    }
+}
