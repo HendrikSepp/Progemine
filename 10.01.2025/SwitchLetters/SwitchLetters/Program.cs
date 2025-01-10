804d14876b5b8abd79621d923c7500110ba9bc6f
Opilane TTHK <opilane@tthk.ee>
2025-01-10 09:31:53 +0200

    init

---
+++ 10.01.2025/SwitchLetters/SwitchLetters/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace SwitchLetters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Palun sisesta vokaal");
+            string sisend = Console.ReadLine();
+
+            
+
+            switch (sisend)
+            {
+                case "A":
+                    Console.WriteLine("Sisestasid tähe A!");
+                    break;
+                case "E":
+                    Console.WriteLine("Sisestasid tähe E!");
+                    break;
+                case "I":
+                    Console.WriteLine("Sisestasid tähe I!");
+                    break;
+                case "O":
+                    Console.WriteLine("Sisestasid tähe O!");
+                    break;
+                case "U":
+                    Console.WriteLine("Sisestasid tähe U!");
+                    break;
+            }
+
+        }
+     
+
+    }
+}
