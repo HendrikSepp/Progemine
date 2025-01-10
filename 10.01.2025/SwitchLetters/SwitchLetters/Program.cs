58d81127e093a7ba4f46ddfbdbf592cc24b017d3
Opilane TTHK <opilane@tthk.ee>
2025-01-10 09:35:44 +0200

    Update Program.cs

--- 10.01.2025/SwitchLetters/SwitchLetters/Program.cs
+++ 10.01.2025/SwitchLetters/SwitchLetters/Program.cs
@@ -13,20 +13,32 @@ namespace SwitchLetters
 
             switch (sisend)
             {
-                case "A":
+                case "A": case "a":
                     Console.WriteLine("Sisestasid tähe A!");
                     break;
-                case "E":
+                case "E": case "e":
                     Console.WriteLine("Sisestasid tähe E!");
                     break;
-                case "I":
+                case "I": case "i":
                     Console.WriteLine("Sisestasid tähe I!");
                     break;
-                case "O":
+                case "O": case "o":
                     Console.WriteLine("Sisestasid tähe O!");
                     break;
-                case "U":
+                case "U": case "u":
                     Console.WriteLine("Sisestasid tähe U!");
+                    break;
+                case "Ä": case "ä":
+                    Console.WriteLine("Sisestasid tähe Ä!");
+                    break;
+                case "Ü": case "ü":
+                    Console.WriteLine("Sisestasid tähe Ü!");
+                    break;
+                case "Õ": case "õ":
+                    Console.WriteLine("Sisestasid tähe Õ!");
+                    break;
+                case "Ö": case "ö":
+                    Console.WriteLine("Sisestasid tähe Ö!");
                     break;
             }
 
