15920a19158016a1fd2392949edd0321f0be1488
Opilane TTHK <opilane@tthk.ee>
2025-01-10 10:07:39 +0200

    init

---
+++ 10.01.2025/RandomNumber/RandomNumber/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Security.Cryptography;
+
+namespace RandomNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = new Random().Next(1, 6);
+            switch (number)
+            {
+                case 1:
+                    Console.WriteLine("Veeretasid {0}!", number);
+                    break;
+                case 2:
+                    Console.WriteLine("Veeretasid {0}!", number);
+                    break;
+                case 3:
+                    Console.WriteLine("Veeretasid {0}!", number);
+                    break;
+                case 4:
+                    Console.WriteLine("Veeretasid {0}!", number);
+                    break;
+                case 5:
+                    Console.WriteLine("Veeretasid {0}!", number);
+                    break;
+                case 6:
+                    Console.WriteLine("Veeretasid {0}!", number);
+                    break;
+
+            }
+        }
+    }
+}
