c2f7f9ea848be6a54b49ad4b1d28e72213f4f8ec
Opilane TTHK <opilane@tthk.ee>
2025-01-16 13:54:02 +0200

    init

---
+++ 16.01.2025/TryCatchSwitch/TryCatchSwitch/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace TryCatchSwitch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Palun sisesta number 1 või 2");
+            try
+            {
+                int number = int.Parse(Console.ReadLine());
+                switch (number)
+                {
+                    case 1:
+                        Console.WriteLine("Number 1! hea valik!");
+                        break;
+                    case 2:
+                        Console.WriteLine("Number 2! hea valik!");
+                        break;
+
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Ei sisestanud numbri formaati");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("ERROR. Sisestatud number on liiga suur");
+            }
+
+        }
+    }
+}
