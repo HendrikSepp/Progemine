fcdc8c8ac3358edb769a1a6b0dd691aedfbec493
Opilane TTHK <opilane@tthk.ee>
2025-01-16 14:32:17 +0200

    init

---
+++ 16.01.2025/SwitchMethodCall/SwitchMethodCall/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace SwitchMethodCall
+{
+    internal class Program
+    {
+        static void Puuvili()
+        {
+            Console.WriteLine("Valisid puuvilja!");
+        }
+        static void Juurvili()
+        {
+            Console.WriteLine("Valisid juurvilja!");
+        }
+        static void Liha()
+        {
+            Console.WriteLine("Valisid liha"!);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Palun vali toit!");
+            Console.WriteLine("Menüüs on:");
+            Console.WriteLine("Kartul, Kaalikas, Õun, Pirn, Kana, Siga");
+            Console.WriteLine("Sisesta oma valik nüüd:");
+            string valik = (Console.ReadLine());
+            switch (valik)
+            {
+                case "Kartul":
+                case "Kaalikas":
+                    Juurvili();
+                    break;
+
+                case "Õun":
+                case "Pirn":
+                    Puuvili();
+                    break;
+
+                case "Kana":
+                case "Siga":
+                    Liha();
+                    break;
+
+                default:
+                    Console.WriteLine("ERROR! tee kindlaks, et alustad valikut suurtähega");
+                    break;
+            }
+
+            }
+    }
+}
