9ddee1fa3e3eaa86ecdf27f8182c6dba1b2c6842
Opilane TTHK <opilane@tthk.ee>
2025-01-16 14:58:45 +0200

    init

---
+++ 16.01.2025/WhileDo/WhileDo/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace WhileDo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("While and Do");
+
+            int number = 1;
+
+            do
+            {
+                Console.WriteLine(number);
+                number++;
+            }
+            while (number <= 5);
+            Console.WriteLine("Numbrid ühest viieni!");
+
+        }
+    }
+}
