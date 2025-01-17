2d45fbf1cbb808e7179ce75adf9c1a8fd822f1ef
Opilane TTHK <opilane@tthk.ee>
2025-01-17 09:33:56 +0200

    init

---
+++ 17.01.2025/M-Koer/M-Koer/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace M_Koer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Tere tulemast M-Koer kalkulaatorprogrammi!");
+            try
+            {
+                Console.WriteLine("Sisesta esimene arv:");
+                float esimene = float.Parse(Console.ReadLine());
+                Console.WriteLine("Sisesta tehtemärk (+ - / *):");
+                string tehe = Console.ReadLine();
+                Console.WriteLine("Sisesta teine arv:");
+                float teine = float.Parse(Console.ReadLine());
+
+
+                switch (tehe)
+                {
+                    case "+":
+                        Console.WriteLine("{0} + {1} = {2}", esimene, teine, esimene + teine);
+                        break;
+
+                    case "-":
+                        Console.WriteLine("{0} - {1} = {2}", esimene, teine, esimene - teine);
+                        break;
+
+                    case "/":
+                        Console.WriteLine("{0} / {1} = {2}", esimene, teine, esimene / teine);
+                        break;
+
+                    case "*":
+                        Console.WriteLine("{0} * {1} = {2}", esimene, teine, esimene * teine);
+                        break;
+
+                    default:
+                        Console.WriteLine("Midagi läks valesti! Kontrolli, kas sisestasid ühe neljast lubatud tehtemärgist");
+                        break;
+                }
+            }
+
+            catch (FormatException)
+            {
+                Console.WriteLine("Ainult numbrid, palun");
+            }
+
+            Console.WriteLine("auh auh auh auh auh");
+        }
+    }
+}
