867390d3df83596a38d2913a9a46ab64b6ca1cd9
Opilane TTHK <opilane@tthk.ee>
2025-01-24 08:52:06 +0200

    init

---
+++ 24.01.2025/ForUshort/ForUshort/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ForUshort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ushort length = 63000;
+            for (int i = 0; i <= length; i++)
+            {
+                Console.WriteLine("i väärtus on: {0}", i);
+            }
+        }
+    }
+}
