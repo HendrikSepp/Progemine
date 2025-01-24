d4738f898371e44bca4f0bd155b85e0833c007c3
Opilane TTHK <opilane@tthk.ee>
2025-01-24 10:35:50 +0200

    init

---
+++ 24.01.2025/ForOddNumbers/ForOddNumbers/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ForOddNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            int i, n, sum = 0;
+
+            Console.WriteLine("Näita summat paaritutest arvudest:\n");
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine("\n\n");
+
+            Console.WriteLine("Sisesta number:");
+
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Paaritud arvud on: ");
+            for (i = 1; i <= n; i++)
+            {
+                Console.Write("{0} ", 2 * i - 1);
+                sum += 2 * i -1;
+            }
+            Console.WriteLine("\nEsimese {0} paaritu arvu summa on: {1}", n, sum);
+        }
+    }
+}
