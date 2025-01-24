f052a6977f73a8865acd8e6f0d6ea3b91245efb6
Opilane TTHK <opilane@tthk.ee>
2025-01-24 09:02:57 +0200

    Update Program.cs

--- 24.01.2025/ForUshort/ForUshort/Program.cs
+++ 24.01.2025/ForUshort/ForUshort/Program.cs
@@ -4,10 +4,13 @@
     {
         static void Main(string[] args)
         {
-            ushort length = 63000;
-            for (int i = 0; i <= length; i++)
+            ushort i = 0;
+            for (;;)
             {
                 Console.WriteLine("i väärtus on: {0}", i);
+                i++;
+                
+             
             }
         }
     }
