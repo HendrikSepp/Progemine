bbaf6297198a30880d2c5b5ca5f2a6128d1f9852
HendrikSepp <sepphendrik@gmail.com>
2025-01-30 16:33:05 +0200

    Update Program.cs

--- 30.01.2025/ForSquare/ForSquare/Program.cs
+++ 30.01.2025/ForSquare/ForSquare/Program.cs
@@ -1,10 +1,46 @@
-﻿namespace ForSquare
+﻿using System.Drawing;
+
+namespace ForSquare
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Draw()
         {
-            Console.WriteLine("Hello, World!");
+            int i, j, side;
+            Console.WriteLine("Sisesta külje pikkus:");
+            side = Convert.ToInt32(Console.ReadLine());
+            for (i = 1; i <= side; i++)
+            {
+                for (j = 1; j <= side; j++)
+                {
+                    Console.Write("* ");
+                }
+
+                Console.Write("\n");
+            }
         }
+            static void Main(string[] args)
+        {
+
+            Console.WriteLine("Kas soovid joonistada ruudu? Y/N");
+            string vastus = Console.ReadLine();
+            switch (vastus) 
+            {
+                    case "Y":
+                        Draw();
+                        break;
+
+                    case "N":
+                        Console.WriteLine("Siis ei joonista täna! Head päeva jätku!");
+                        break;
+
+                    default:
+                        Console.WriteLine("ainult Y/N vastused palun");
+                    break;
+
+
+            }
+        }
+
     }
 }
