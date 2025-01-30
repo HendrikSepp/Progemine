d608b4799ecf7440689d8e470096244304a94131
HendrikSepp <sepphendrik@gmail.com>
2025-01-30 16:06:55 +0200

    init

---
+++ 30.01.2025/ForLoopTriangle3/ForLoopTriangle3/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ForLoopTriangle3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Veel üks kolmnurk numbritest");
+
+            int i, j, k = 0, rows;
+
+            Console.WriteLine("Sisesta ridade arv:");
+            rows = Convert.ToInt32(Console.ReadLine());
+            for (i = 1; i <= rows; i++)
+            {
+                for (j = 1; j <= i; j++)
+                {
+                    Console.Write("{0} ", k++);
+                }
+
+                Console.Write("\n");
+            }
+        }
+    }
+}
