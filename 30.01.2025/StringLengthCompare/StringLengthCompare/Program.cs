f3ff162580bec1cec7f9e3b7993ff12ebc1a4fef
HendrikSepp <sepphendrik@gmail.com>
2025-01-30 17:29:57 +0200

    init

---
+++ 30.01.2025/StringLengthCompare/StringLengthCompare/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace StringLengthCompare
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kellel on kõige pikem nimi?\n");
+            string[] animalName = { "Koer", "Kass", "Jänes", "Sipelgasiil", "Lendorav" };
+
+            string longestName = "";
+            int longestLength = 0;
+
+            foreach (string animal in animalName)
+            {
+                Console.WriteLine("{0} on {1} tähemärki pikk\n", animal, animal.Length);
+
+                if (animal.Length > longestLength)
+                {
+                    longestName = animal;
+                    longestLength = animal.Length;
+                }
+            }
+
+
+            Console.WriteLine("Kõige pikem nimi on {0}, {1} tähemärki pikk!", longestName, longestLength);
+        }
+    }
+}
