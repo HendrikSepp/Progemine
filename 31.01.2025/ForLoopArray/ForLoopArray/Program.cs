1a2bd43f58ff37905f1d53eea4280b46f17e3666
Opilane TTHK <opilane@tthk.ee>
2025-01-31 10:40:18 +0200

    init

---
+++ 31.01.2025/ForLoopArray/ForLoopArray/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ForLoopArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string[] myArray = { "H", "e", "l", "l", "o" };
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                Console.WriteLine(myArray[i]);
+            }
+        }
+    }
+}
