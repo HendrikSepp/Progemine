e3040771d1278758bf7d574d60d65994e5a68a47
Opilane TTHK <opilane@tthk.ee>
2025-01-31 11:15:13 +0200

    init

---
+++ 31.01.2025/ForEachSorting/ForEachSorting/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ForEachSorting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string[] genders = { "m", "f", "m", "m", "m", "f", "f", "m", "f" };
+            int m = 0, f = 0;
+            foreach (string gender in genders)
+            {
+                if (gender == "m")
+                {
+                    m++;
+                }
+                if (gender == "f")
+                {
+                    f++;
+                }
+            }
+            Console.WriteLine("Number of male: " + m);
+            Console.WriteLine("number of female: " + f);
+        }
+    }
+}
