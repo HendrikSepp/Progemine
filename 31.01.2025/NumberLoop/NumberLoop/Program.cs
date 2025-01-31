df951dea58af34892ce23f1d3f68f8bac71e3008
Opilane TTHK <opilane@tthk.ee>
2025-01-31 12:19:14 +0200

    init

---
+++ 31.01.2025/NumberLoop/NumberLoop/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace NumberLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Foreach and for loop");
+
+            int[] oddArray = new int[] { 1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21 };
+
+            foreach (int i in oddArray)
+            {
+                if (i <= 13)
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
