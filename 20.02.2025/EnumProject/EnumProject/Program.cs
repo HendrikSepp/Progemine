fb4ecd5db3bc07f5b1623b853faac5063e5db51e
HendrikSepp <sepphendrik@gmail.com>
2025-02-20 17:28:09 +0200

    init

---
+++ 20.02.2025/EnumProject/EnumProject/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace EnumProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enum!");
+            Console.WriteLine(Weekdays.Monday);
+            Console.WriteLine((int) Weekdays.Monday);
+
+        }
+
+        enum Weekdays
+        {
+            Monday,
+            Tuesday, 
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+    }
+}
