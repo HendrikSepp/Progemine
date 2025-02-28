6ae7f5fb2606a726cede0f417af6cf020c774c8c
Opilane TTHK <opilane@tthk.ee>
2025-02-28 12:23:24 +0200

    init

---
+++ 28.02.2025/LINQ/LINQ/PeopleList.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    public class PeopleList
+    {
+        public static readonly List<People> people = new List<People>
+        {
+            new People()
+            {
+                Id = 1,
+                Name = "Joonas",
+                Age = 33,
+                GenderId = Guid.Parse("04acd742-caf4-4325-82e0-6f357f869374")
+            },
+            new People()
+            {
+                Id = 2,
+                Name = "Moona",
+                Age = 21,
+                GenderId = Guid.Parse("b5984042-4499-409e-adc5-b741e68d2ac7")
+            },
+            new People()
+            {
+                Id = 3,
+                Name = "Ron",
+                Age = 18,
+                GenderId = Guid.Parse("04acd742-caf4-4325-82e0-6f357f869374")
+            },
+            new People()
+            {
+                Id = 4,
+                Name = "Jane",
+                Age = 20,
+                GenderId = Guid.Parse("b5984042-4499-409e-adc5-b741e68d2ac7")
+            },
+            new People()
+            {
+                Id = 5,
+                Name = "Mari",
+                Age = 17,
+                GenderId = Guid.Parse("b5984042-4499-409e-adc5-b741e68d2ac7")
+            },
+            new People()
+            {
+                Id = 6,
+                Name = "Mari",
+                Age = 33,
+                GenderId = Guid.Parse("b5984042-4499-409e-adc5-b741e68d2ac7")
+            }
+        };
+    }
+}
---
+++ 28.02.2025/LINQ/LINQ/Program.cs
@@ -0,0 +1,169 @@
+﻿namespace LINQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tee valik numbriga");
+            Console.WriteLine("1 Sum LINQ");
+            Console.WriteLine("2 Max LINQ");
+            Console.WriteLine("3 Aggregate LINQ");
+            Console.WriteLine("4 Min LINQ");
+            Console.WriteLine("5 Skip LINQ");
+            Console.WriteLine("6 SkipWhile LINQ");
+            Console.WriteLine("7 Take LINQ");
+            Console.WriteLine("8 TakeWhile LINQ");
+            Console.WriteLine("9 FirstOrDefault LINQ");
+
+
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    SumLINQ();
+                    break;
+
+                case "2":
+                    MaxLINQ();
+                    break;
+
+                case "3":
+                    AggregateLINQ();
+                    break;
+
+                case "4":
+                    MinLINQ();
+                    break;
+
+                case "5":
+                    SkipLINQ();
+                    break;
+
+                case "6":
+                    SkipWhileLINQ();
+                    break;
+
+                case "7":
+                    TakeLINQ();
+                    break;
+
+                case "8":
+                    TakeWhileLINQ();
+                    break;
+
+                case "9":
+                    FirstOrDefaultLINQ();
+                    break;
+
+                default:
+                    Console.WriteLine("Error, proovi uuesti");
+                    break;
+            }
+        }
+        
+        public static void SumLINQ()
+        {
+            var sumAge = PeopleList.people.Sum(x => x.Age);
+
+            Console.WriteLine(sumAge);
+
+            Console.WriteLine("Täisealiste isikute koondarv");
+
+            var sumAdults = PeopleList.people.Sum(x =>
+            {
+                if (x.Age >= 18)
+                {
+                    return x.Age;
+                }
+                else
+                {
+                    return 0;
+                }
+            });
+
+            Console.WriteLine("Täiskasvanud isikute summa on:" + sumAdults);
+        }
+
+        public static void MaxLINQ()
+        {
+            var oldestPerson = PeopleList.people
+                .Max(x => x.Age);
+
+            Console.WriteLine("Kõige vanem inimene on {0} aastane", oldestPerson);
+        }
+
+        public static void AggregateLINQ()
+        {
+            string commaSeparatedPeoplesNames = PeopleList.people
+                .Aggregate<People, string>(
+                "Peoples names: ",
+                (str, y) => str += y.Name + ", "
+                );
+
+            Console.WriteLine(commaSeparatedPeoplesNames);
+        }
+
+        public static void MinLINQ()
+        {
+            var youngestPerson = PeopleList.people
+                .Min(x => x.Age);
+
+            Console.WriteLine("Kõige noorem inimene on {0} aastane", youngestPerson);
+        }
+
+        public static void SkipLINQ()
+        {
+            var skipThree = PeopleList.people.Skip(3);
+
+            foreach (var person in skipThree)
+            {
+                Console.WriteLine(person.Name);
+            }
+        }
+
+        public static void SkipWhileLINQ()
+        {
+            var SkipWhileId = PeopleList.people.SkipWhile(x => x.Id < 4);
+
+            foreach (var person in  SkipWhileId)
+            {
+                Console.WriteLine(person.Name);
+            }
+        }
+
+        public static void TakeLINQ()
+        {
+            var TakeThree = PeopleList.people.Take(3);
+
+            foreach(var person in TakeThree)
+            {
+                Console.WriteLine(person.Name); 
+            }
+        }
+        
+
+        public static void TakeWhileLINQ()
+        {
+            var TakeFirstThree = PeopleList.people.TakeWhile(x => x.Id < 4);
+            foreach(var person in TakeFirstThree)
+            {
+                Console.WriteLine(person.Name);
+            }
+        }
+
+        public static void FirstOrDefaultLINQ()
+        {
+            var FOD = PeopleList.people.FirstOrDefault(x => x.Age < 20);
+
+            if (FOD != null)
+            {
+                Console.WriteLine("Esimene alla 20 aastane on: {0}", FOD.Name);
+            }
+            else
+            {
+                Console.WriteLine("Keegi pole alla 20 aastane");
+            }
+        }
+    }
+}
