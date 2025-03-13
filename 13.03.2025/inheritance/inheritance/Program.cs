532655774199ad2399dcf23d0c51c33ec76ed775
Opilane TTHK <opilane@tthk.ee>
2025-03-13 17:09:49 +0200

    init

---
+++ 13.03.2025/inheritance/inheritance/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Drawing;
+
+namespace inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta esimene number");
+            int firstNr = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Sisesta teine number");
+            int secondNr = Convert.ToInt32(Console.ReadLine());
+
+            Rectangle rectangle = new Rectangle();
+            rectangle.SetWidth(firstNr);
+            rectangle.SetHeight(secondNr);
+
+            Console.WriteLine("Pindala on: {0}", rectangle.GetArea());
+        }
+    }
+
+    class Shape
+    {
+        public void SetWidth(int w)
+        {
+            width = w;
+        }
+
+        public void SetHeight(int h)
+        { 
+            height = h;
+        }
+
+        protected int width;
+        protected int height;
+    }
+
+    class Rectangle : Shape
+    {
+        public int GetArea()
+        {
+            return (width * height);
+        }
+    }
+}
---
+++ 13.03.2025/sortedList/sortedList/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Diagnostics.Tracing;
+
+namespace sortedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sorted List!");
+
+            SortedList<int, string> numbrid = new SortedList<int, string>()
+            {
+                {3, "Three" },
+                {5, "Five" },
+                {1, "One" },
+            };
+
+            Console.WriteLine("Algne key value");
+            //sorteerib numbriväärtuste järgi ära
+            foreach (KeyValuePair<int, string> kvp in numbrid)
+            {
+                Console.WriteLine("Key: {0}, value: {1}", kvp.Key, kvp.Value);
+            }
+            Console.WriteLine("---------------------------------------------");
+            SortedList<string, string> linnad = new SortedList<string, string>()
+            {
+                {"Tallinn", "Harjumaa" },
+                {"Tartu", "Tartumaa" },
+                {"Haapsalu", "Läänemaa" },
+                {"Pärnu", "Pärnumaa" },
+                {"Valga", "Valgamaa" },
+                {"Narva", "Ida-Virumaa" }
+            };
+            Console.WriteLine("Linnad ja maakonnad:");
+
+            foreach (KeyValuePair<string, string> kvp in linnad)
+            {
+                Console.WriteLine("Key: {0}, value: {1}", kvp.Key, kvp.Value);
+            }
+
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine("Ilma SortedList kasutamata");
+            linnad.Add("New York", "USA");
+            linnad.Add("Brazil", "BRA");
+
+            foreach (KeyValuePair<string, string> kvp in linnad)
+            {
+                Console.WriteLine("Key: {0}, value: {1}", kvp.Key, kvp.Value);
+            }
+
+
+        }
+    }
+}
